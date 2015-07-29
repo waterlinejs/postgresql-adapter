c665dd8dd3ab81bc8ead247e780efe0012d129ed
Travis Webb <me@traviswebb.com>
2015-07-29 01:18:36 -0400

    remove gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-
-gulp.task('build', function () {
-  return gulp.src([ 'lib/**' ])
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
