4d286e425f201eb41e6d34eabcff93cbaec816fd
Travis Webb <me@traviswebb.com>
2016-02-19 18:16:53 -0500

    [test] add edgecase tests for json/array types

---
+++ test/integration/pg/models.js
@@ -0,0 +1,46 @@
+const Waterline = require('waterline')
+
+module.exports = {
+
+  ArrayModel: Waterline.Collection.extend({
+    identity: 'arraymodel',
+    connection: 'edgetests',
+    dynamicFinders: false,
+    associationFinders: false,
+
+    attributes: {
+      list: {
+        type: 'array'
+      },
+      listSyntaxA: {
+        type: 'array',
+        defaultsTo: '{}'
+      }
+    }
+  }),
+
+  JsonModel: Waterline.Collection.extend({
+    identity: 'jsonmodel',
+    connection: 'edgetests',
+    dynamicFinders: false,
+    associationFinders: false,
+
+    attributes: {
+      json: {
+        type: 'json'
+      },
+      jsonb: {
+        type: 'json'
+      },
+      jsonbSyntaxA: {
+        type: 'json',
+        defaultsTo: '[]'
+      },
+      jsonbSyntaxB: {
+        type: 'json',
+        defaultsTo: '{}'
+      }
+    }
+  })
+
+}
